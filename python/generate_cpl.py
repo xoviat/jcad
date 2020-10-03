239743731b3c7131daec16a843a5dbd9418bcea6
xoviat <xoviat@users.noreply.github.com>
2020-10-03 18:31:49 -0500

    generate cpl file

--- python/generate_cpl.py
+++ python/generate_cpl.py
@@ -43,6 +43,17 @@ def pad_sort_key(name):
 
 
 def convert(pcb, brd):
+    IU_PER_MM = PCB_IU_PER_MM = 1e6
+    IU_PER_MILS = (IU_PER_MM * 0.0254)
+
+    conv_unit_inch = 0.001 / IU_PER_MILS
+    conv_unit_mm = 1.0 / IU_PER_MM
+
+    units = pcbnew.GetUserUnits()
+
+    # TODO: select units from board somehow
+    conv_unit = conv_unit_mm
+
     # Board outline
     outlines = pcbnew.SHAPE_POLY_SET()
     pcb.GetBoardPolygonOutlines(outlines, "")
@@ -50,36 +61,7 @@ def convert(pcb, brd):
     outline_points = [outline.Point(n) for n in range(outline.PointCount())]
     outline_maxx = max(map(lambda p: p.x, outline_points))
     outline_maxy = max(map(lambda p: p.y, outline_points))
-
-    brd.write("0\n")  # unknown
-
-    brd.write(
-        "BRDOUT: {count} {width} {height}\n".format(
-            count=len(outline_points) + outline.IsClosed(),
-            width=coord(outline_maxx),
-            height=coord(outline_maxy),
-        )
-    )
-    for point in outline_points:
-        brd.write("{x} {y}\n".format(x=coord(point.x), y=coord(point.y)))
-    if outline.IsClosed():
-        brd.write(
-            "{x} {y}\n".format(
-                x=coord(outline_points[0].x), y=coord(outline_points[0].y)
-            )
-        )
-    brd.write("\n")
-
-    # Nets
-    net_info = pcb.GetNetInfo()
-    net_items = [net_info.GetNetItem(n) for n in range(1, net_info.GetNetCount())]
-
-    brd.write("NETS: {count}\n".format(count=len(net_items)))
-    for net_item in net_items:
-        brd.write(
-            "{code} {name}\n".format(code=net_item.GetNet(), name=net_item.GetNetname())
-        )
-    brd.write("\n")
+    m_place_offset = pcb.GetDesignSettings().m_AuxOrigin
 
     # Parts
     module_list = pcb.GetModules()
@@ -89,69 +71,34 @@ def convert(pcb, brd):
             modules.append(module_list)
         module_list = module_list.Next()
 
-    brd.write("PARTS: {count}\n".format(count=len(modules)))
     pin_at = 0
+
+    # Logic taken from pcbnew/exporters/export_footprints_placefile.cpp
+    # See https://gitlab.com/kicad/code/kicad/-/issues/2453
     for module in modules:
+        footprint_pos = module.GetPosition()
+        footprint_pos -= m_place_offset
+
+        layer = module.GetLayer()
+        if(layer not in (pcbnew.B_Cu, pcbnew.F_Cu)):
+            continue
+
+        if(layer == pcbnew.B_Cu):
+            footprint_pos.x = - footprint_pos.x
+
         module_bbox = module.GetBoundingBox()
         brd.write(
-            "{ref} {x1} {y1} {x2} {y2} {pin} {side}\n".format(
+            "{ref}	{value}	{name}	{x0}	{y0}	{orientation}	{layer}\n".format(
                 ref=module.GetReference(),
-                x1=coord(module_bbox.GetLeft()),
-                y1=y_coord(module, outline_maxy, module_bbox.GetTop()),
-                x2=coord(module_bbox.GetRight()),
-                y2=y_coord(module, outline_maxy, module_bbox.GetBottom()),
-                pin=pin_at,
-                side=1 + module.IsFlipped(),
+                value=module.GetValue(),
+                name=module.GetFPID().GetLibItemName(),
+                x0=footprint_pos.x * conv_unit,
+                y0=-footprint_pos.y * conv_unit,
+                orientation=module.GetOrientation() / 10.0,
+                layer="top" if layer == pcbnew.F_Cu else "bottom",
             )
         )
         pin_at += module.GetPadCount()
-    brd.write("\n")
-
-    # Pins
-    module_list = pcb.GetModules()
-    pads = []
-    while module_list:
-        if not skip_module(module_list):
-            pads_list = module_list.PadsList()
-            for pad in sorted(pads_list, key=lambda pad: pad_sort_key(pad.GetName())):
-                pads.append(pad)
-        module_list = module_list.Next()
-
-    brd.write("PINS: {count}\n".format(count=len(pads)))
-    for pad in pads:
-        pad_pos = pad.GetPosition()
-        brd.write(
-            "{x} {y} {net} {side}\n".format(
-                x=coord(pad_pos.x),
-                y=y_coord(pad, outline_maxy, pad_pos.y),
-                net=pad.GetNetCode(),
-                side=1 + pad.IsFlipped(),
-            )
-        )
-    brd.write("\n")
-
-    # Nails
-    module_list = pcb.GetModules()
-    testpoints = []
-    while module_list:
-        if not skip_module(module_list, tp=True):
-            pads_list = module_list.PadsList()
-            for pad in sorted(pads_list, key=lambda pad: pad_sort_key(pad.GetName())):
-                testpoints.append((module_list, pad))
-        module_list = module_list.Next()
-
-    brd.write("NAILS: {count}\n".format(count=len(testpoints)))
-    for module, pad in testpoints:
-        pad_pos = pad.GetPosition()
-        brd.write(
-            "{probe} {x} {y} {net} {side}\n".format(
-                probe=module.GetReference()[2:],
-                x=coord(pad_pos.x),
-                y=y_coord(pad, outline_maxy, pad_pos.y),
-                net=pad.GetNetCode(),
-                side=1 + pad.IsFlipped(),
-            )
-        )
     brd.write("\n")
 
 
@@ -167,7 +114,7 @@ def main():
         "brd_file",
         metavar="BRD-FILE",
         type=argparse.FileType("wt"),
-        help="output in .brd format",
+        help="output in .cpl format",
     )
 
     args = parser.parse_args()

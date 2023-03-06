d1c32eeea57facf079b262bd15706431a803cffc
xoviat <xoviat@users.noreply.github.com>
2023-03-06 17:39:58 -0600

    Add draft jlc component api

---
+++ lib/jlc.go
@@ -0,0 +1,26 @@
+package lib
+
+func JLCGetComponentDetail() {
+	/* https://jlcpcb.com/shoppingCart/smtGood/getComponentDetail?componentLcscId=1899 */
+}
+
+func JLCSelectComponentList() {
+	/* https://jlcpcb.com/shoppingCart/smtGood/selectSmtComponentList */
+
+	/*
+		componentAttributes	:  []
+		componentBrand	: 	null
+		componentLibraryType	: 	"base"
+		componentSpecification	: 	null
+		currentPage	: 	2
+		firstSortId	: 	""
+		firstSortName	: 	""
+		firstSortNameNew	: 	""
+		keyword	: 	null
+		pageSize	: 	25
+		searchSource	: 	"search"
+		secondSortName	: 	""
+		stockFlag	: 	null
+		stockSort	: 	null
+	*/
+}
